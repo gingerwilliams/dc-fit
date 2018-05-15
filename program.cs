e52c4bda40d486e53744b1009883678a4a71ed74
Ginger Williams <gingerwilliams@Gingers-MacBook-Pro.local>
2018-05-14 21:49:45 -0400

    init

---
+++ program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DigitalConduct.Fit
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Prompt the user for minutes excercised 
+            // System.Console.Write("Enter how many minutes you exercided: ");
+            Console.Write("Enter how many minutes you exercided: ");
+        
+            string entry = Console.ReadLine();
+            //Add minutes exercied to total
+            //Display total min excerised on the screen
+            //Repeat until the user quits
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+<!-- 
+
+mcs Program.cs                                               
+> ls                                                           
+Program.cs  Program.exe                                                             
+mono Program.exe                                             
+> Enter how many minutes you exercided:  
+
+-->
